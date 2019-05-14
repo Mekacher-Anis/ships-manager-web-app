5e9c6349555939e82cba3a3788b9491a47c18783
Anis Mekacher <haithem1297@outlook.fr>
2019-05-14 03:19:11 +0200

    added favicon

--- pages/category-selection.php
+++ pages/category-selection.php
@@ -22,6 +22,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Ships Manager</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
 
     <link rel="stylesheet" href="../styles/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="../styles/main.css">
